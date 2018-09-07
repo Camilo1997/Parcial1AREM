729934383089f22422a726ba6a68c59d1a077f53
Rafael <daniela.gonzalezramirez@hotmail.com>
2018-09-07 15:41:47 -0500

    Proyecto terminado

--- src/main/java/edu/escuelaing/arem/javaClient.java
+++ src/main/java/edu/escuelaing/arem/javaClient.java
@@ -6,9 +6,13 @@
 package edu.escuelaing.arem;
 
 import java.awt.Desktop;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,12 +25,19 @@ public class javaClient {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws URISyntaxException, IOException {
-
-        if (Desktop.isDesktopSupported()) {
-            String varNumbers = "numbers=1+2+3+4+5+6+"; //El simbolo + al final es necesario
-            Desktop.getDesktop().browse(new URI("https://mighty-scrubland-70358.herokuapp.com/results?" + varNumbers));
+    public static void main(String[] args) throws MalformedURLException {
+        String numbersPage = "numbers=1+2+3+4+5+6+7+8+"; // El simbolo + al final de los numeros es necesario
+        URL pageHeroku = new URL("https://mighty-scrubland-70358.herokuapp.com/results?" + numbersPage);
+        try (BufferedReader reader
+                = new BufferedReader(new InputStreamReader(pageHeroku.openStream()))) {
+            String inputLine = null;
+            while ((inputLine = reader.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        } catch (IOException x) {
+            System.err.println(x);
         }
+
     }
 
 }
